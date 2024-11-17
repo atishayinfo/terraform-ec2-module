7c72eed1d834742d5d96c251d14e0b04f2ce9967
Atishay <atishay2024@outlook.com>
2024-11-17 18:29:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,9 +7,13 @@ resource "aws_instance" "ec2_instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
   key_name      = var.key_name
-  tags = {
-    Name = var.instance_name
-  }
+
+  tags = merge(
+    {
+      Name = var.instance_name
+    },
+    var.additional_tags
+  )
 
   # Optional: Security group for the instance
   vpc_security_group_ids = [var.security_group_id]
