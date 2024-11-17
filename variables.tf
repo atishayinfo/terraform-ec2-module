1ef23eb3710985ae6383a8e9d1bd1132f88b471f
Atishay <atishay2024@outlook.com>
2024-11-17 14:31:15 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,19 +1,46 @@
+# variables.tf
+
 variable "region" {
-  description = "AWS region"
+  description = "The AWS region to launch the instance"
   type        = string
+  default     = "us-east-1"
 }
 
 variable "ami_id" {
-  description = "AMI ID for the instance"
+  description = "AMI ID for the EC2 instance"
   type        = string
 }
 
 variable "instance_type" {
-  description = "Instance type"
+  description = "Type of EC2 instance"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "Key name for SSH access"
   type        = string
 }
 
 variable "instance_name" {
-  description = "Name for the EC2 instance"
+  description = "Name of the EC2 instance"
   type        = string
+  default     = "my-ec2-instance"
+}
+
+variable "security_group_id" {
+  description = "Security group ID to assign to the instance"
+  type        = string
+}
+
+variable "volume_size" {
+  description = "Size of the root volume (in GB)"
+  type        = number
+  default     = 8
+}
+
+variable "volume_type" {
+  description = "Volume type (e.g., gp2, io1)"
+  type        = string
+  default     = "gp2"
 }
