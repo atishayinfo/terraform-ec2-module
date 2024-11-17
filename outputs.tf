f551a58a331738ea9ba4b134a080fa5135dcdc27
Atishay <atishay2024@outlook.com>
2024-11-17 14:31:37 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,10 @@
+# outputs.tf
 output "instance_id" {
-  description = "The ID of the EC2 instance"
+  description = "The instance ID of the EC2 instance"
   value       = aws_instance.ec2_instance.id
+}
+
+output "instance_public_ip" {
+  description = "The public IP of the EC2 instance"
+  value       = aws_instance.ec2_instance.public_ip
 }
